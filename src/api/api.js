0c362c3ab47468eff42e79819cb166b26d72f366
fahimhussain <fahim.hussain@pw.live>
2025-09-16 00:04:32 +0530

    inital setup

---
+++ src/api/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+    // baseURL: `${import.meta.end.VITE_BACK_END_URL}/api` ,
+      baseURL: `${import.meta.env.VITE_BACK_END_URL}/api`,
+
+
+});
+
+export default api;
